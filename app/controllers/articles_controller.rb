e10a5d45fcbe962b6ff98ce9b0063384bf9ffb4e
Josias Alvarado <josiasjag@gmail.com>
2020-12-08 11:29:18 -0600

    Add flash for methods #create, #destroy and #update.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,25 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    flash.notice = "Article '#{@article.title}' saved!"
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash.notice = "Article '#{@article.title}' deleted!"
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    flash.notice = "Article '#{@article.title}' updated!"
     redirect_to article_path(@article)
   end
 end
