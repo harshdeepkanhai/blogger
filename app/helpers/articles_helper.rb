e7924fe9553622bd633b71e2d0fc68828d593ff3
Josias Alvarado <josiasjag@gmail.com>
2020-12-08 15:50:27 -0600

    Add methods and model for tagging system.

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,4 +2,8 @@ module ArticlesHelper
   def article_params
     params.require(:article).permit(:title, :body)
   end
+
+  def article_params
+    params.require(:article).permit(:title, :body, :tag_list)
+  end
 end
