4f52f7363e22b2525ddd89e224c3f0ea6c047362
Josias Alvarado <josiasjag@gmail.com>
2020-12-09 13:37:34 -0600

    Add Tag view and deletion functionality.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,14 +1,14 @@
 class Article < ApplicationRecord
   has_many :comments
-  has_many :taggings
-  has_many :tags, dependent: :destroy, through: :taggings
+  has_many :taggings, dependent: :destroy
+  has_many :tags, through: :taggings
 
   def tag_list
     tags.join(', ')
   end
 
   def tag_list=(tags_string)
-    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+    tag_names = tags_string.split(',').collect{|s| s.strip.downcase}.uniq
     new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
     self.tags = new_or_found_tags
   end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,7 @@
 class Tag < ApplicationRecord
+  has_many :comments
   has_many :taggings
-  has_many :articles, through: :taggings
+  has_many :articles, through: :taggings, dependent: :destroy
 
   def to_s
     name
