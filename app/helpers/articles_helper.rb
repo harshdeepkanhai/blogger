e0837b6d0e17daf0fedce4095c7bc706eaf469d1
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2020-12-10 01:41:54 +0530

    Add image upload functionality with paperclip gem

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -4,6 +4,6 @@ module ArticlesHelper
   end
 
   def article_params
-    params.require(:article).permit(:title, :body, :tag_list)
+    params.require(:article).permit(:title, :body, :tag_list, :image)
   end
 end
